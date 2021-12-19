f2b87dd81d4786635bbd07ff172948df2c585c14
velovan <velovan@users.noreply.github.com>
2021-12-19 15:04:36 +0200

    Update Main.java

--- Tanks/src/main/Main.java
+++ Tanks/src/main/Main.java
@@ -11,14 +11,13 @@ import display.Display;
 import game.Game;
 
 public class Main {
-
 	
 	public static void main(String[] args){
 	
 	Game tanks = new Game();
 	
 	tanks.start();
-	
 		
 	}
+	
 }
