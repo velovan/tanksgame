6a3c5907d1e25b911d59d32af81cfb4c22485071
velovan <velovan@users.noreply.github.com>
2022-06-09 19:05:50 +0300

    Update ResourseLoader.java

--- Tanks/src/utils/ResourseLoader.java
+++ Tanks/src/utils/ResourseLoader.java
@@ -7,21 +7,17 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 /*
-public class ResourseLoader {
+public class ResourseLoader{
 	
 	public static final String PATH = "res/";
 	
 	public static BufferedImage loadImage (String fileName){
-		
 		BufferedImage image = null;
-		
 		try{			
 		   image = ImageIO.read(new File (PATH + fileName));
-			
 		}catch (IOException e){
 			e.printStackTrace();
 		 }
-		
 		return image;
 	}
 }*/
