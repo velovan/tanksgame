bc396f8b05df9290c0633cd5eb6b8e7fcdf04bc6
velovan <velovan@users.noreply.github.com>
2022-07-04 21:05:44 +0300

    Update Main.java

--- Tanks/src/main/Main.java
+++ Tanks/src/main/Main.java
@@ -10,10 +10,9 @@ import javax.swing.Timer;
 import display.Display;
 import game.Game;
 
-public class Main {
+public class Main{
 	
 	public static void main(String[] args){
-	
 	Game tanks = new Game();
 	tanks.start();
 		
