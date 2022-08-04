8415bc1f1aac53641e854df8c08273f3abebae82
velovan <velovan@users.noreply.github.com>
2022-08-04 11:13:29 +0300

    Update TextureAtlas.java

--- Tanks/src/graphics/TextureAtlas.java
+++ Tanks/src/graphics/TextureAtlas.java
@@ -25,12 +25,12 @@ public class TextureAtlas{
 	   
 	 public TextureAtlas(String imageName){
 		 
-		 image = ResourseLoader.loadImage(imageName);
+	       	image = ResourseLoader.loadImage(imageName);
 	 }
 	   
 	 public BufferedImage cut (int x, int y, int w, int h){
 		
-		 return image.getSubimage(x, y, w, h);
+		return image.getSubimage(x, y, w, h);
 	 }
 	   
    }
