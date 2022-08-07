ec255fb216c5cbcd2449e2647bbdc025e0ee6fbd
velovan <velovan@users.noreply.github.com>
2022-08-07 12:43:40 +0300

    Update Time.java

--- Tanks/src/utils/Time.java
+++ Tanks/src/utils/Time.java
@@ -2,10 +2,10 @@ package utils;
 
 public class Tim{
 	
-   public static final long SECOND = 1000000000l; 
+    public static final long SECOND = 1000000000l; 
    
-   public static long get(){
-      return System.nanoTime();
+    public static long get(){
+       return System.nanoTime();
     }
 
 }
