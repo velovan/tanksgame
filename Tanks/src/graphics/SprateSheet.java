d2d8a3c888af512b6b475327676749c390347574
velovan <velovan@users.noreply.github.com>
2022-09-18 10:46:22 +0300

    Update SprateSheet.java

--- Tanks/src/graphics/SprateSheet.java
+++ Tanks/src/graphics/SprateSheet.java
@@ -9,14 +9,14 @@ public class SprateSheet{
 	public int spriteScale;
 	public int spriteWidth;
 	
-	public SprateSheet(BufferedImage sheet, int sprite, int spriteScale){
+	public SprateSheet(BufferedImage sheet, int sprite, int spriteScale) {
 		this.sheet = sheet;
 		this.sprite = sprite;
 		this.spriteScale = spriteScale;
 		this.spriteWidth = sheet.getHeight() / spriteScale;
 	}
 	
-	public BufferedImage getSprite (int index){
+	public BufferedImage getSprite (int index) {
 	    index = index % spriteScale;
 	    int x = index % spriteWidth * spriteScale;
 	    int y = index / spriteWidth * spriteScale;
