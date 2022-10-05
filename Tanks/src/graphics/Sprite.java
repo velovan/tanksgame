359984d384cb0951b2ca34ee709f5f14fe803614
velovan <velovan@users.noreply.github.com>
2022-10-05 10:47:08 +0300

    Update Sprite.java

--- Tanks/src/graphics/Sprite.java
+++ Tanks/src/graphics/Sprite.java
@@ -4,17 +4,17 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 /*
-public class Sprite {
+public class Sprite{
 
    private SprateSheet sheet;
    private float       scale;
    
-   public Sprite (SprateSheet sheet, float scale {
+   public Sprite (SprateSheet sheet, float scale{
 	    this.sheet = sheet;
 	    this.scale = scale;
    }
 
-   public void render (Graphics2D g, float x, float y) { 
+   public void render (Graphics2D g, float x, float y){ 
 	    BufferedImage image = sheet.getSprite(0);
 	    g.drawImage(sheet.getSprite(0),(int)(x), (int)(y),(int)(image.getWidth()*scale), (int) (image.getHeight() * scale),null);   
    }
